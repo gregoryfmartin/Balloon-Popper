88474c74186c56e9a5161bafb038fd6e5a4cabd9
Gregory F Martin <gregoryfrankmartin@icloud.com>
2022-04-27 11:55:12 -0400

    Starting to piece together the title screen. It recognizes touch events on nodes.

--- Balloon Popper/Source/Scene Controllers/TitleScreenScene.swift
+++ Balloon Popper/Source/Scene Controllers/TitleScreenScene.swift
@@ -6,5 +6,34 @@
 //
 
 import Foundation
+import SwiftUI
 
-class TitleScreenScene : GMScene {}
+class TitleScreenScene : GMScene {
+    private var _lastUpdateTime: TimeInterval = 0.0
+    
+    override func sceneDidLoad() {
+        self._lastUpdateTime = 0.0
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
+        
+        if self._lastUpdateTime == 0.0 {
+            self._lastUpdateTime = currentTime
+        }
+        let dt = currentTime - self._lastUpdateTime
+        
+        self.gameMaster.pfsm.update(deltaTime: dt)
+        
+        self._lastUpdateTime = currentTime
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let location = touch.location(in: self)
+        let frontTouchedNode = atPoint(location).name
+        
+        print("Touched node: \(String(describing: frontTouchedNode))")
+    }
+}
