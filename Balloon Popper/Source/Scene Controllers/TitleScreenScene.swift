788603974aaf96aa1163ae934102aa52fc2c32d6
Gregory F Martin <gregoryfrankmartin@icloud.com>
2022-05-01 12:48:46 -0400

    Volume changes seem to work this way.

--- Balloon Popper/Source/Scene Controllers/TitleScreenScene.swift
+++ Balloon Popper/Source/Scene Controllers/TitleScreenScene.swift
@@ -7,12 +7,21 @@
 
 import Foundation
 import SwiftUI // This needs included for the touch event recognition, otherwise UITouch and UIEvent can't be found
+import SpriteKit
 
 class TitleScreenScene : GMScene {
     private var _lastUpdateTime: TimeInterval = 0.0
+    private var _labelOptions: SKLabelNode? = nil
+    private var _labelCredits: SKLabelNode? = nil
+    private var _audioBgm: SKAudioNode = SKAudioNode(fileNamed: "Title Screen Music B")
     
     override func sceneDidLoad() {
         self._lastUpdateTime = 0.0
+        self._labelOptions = self.childNode(withName: "\\labelOptions") as! SKLabelNode?
+        self._labelCredits = self.childNode(withName: "\\labelCredits") as! SKLabelNode?
+        self._audioBgm.autoplayLooped = true
+        self._audioBgm.run(SKAction.changeVolume(to: 0.1, duration: 0.0))
+        self.addChild(self._audioBgm)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -38,7 +47,13 @@ class TitleScreenScene : GMScene {
         let frontTouchedNode = atPoint(location).name
         
         if frontTouchedNode == "labelOptions" {
-            self.gameMaster.pfsm.enter(GameMaster.GMSOptionsScreen.self)
+            self._labelOptions?.run(SKAction.sequence([
+                SKAction.playSoundFileNamed("Voice Options", waitForCompletion: true),
+                SKAction.run {
+                    self.gameMaster.pfsm.enter(GameMaster.GMSOptionsScreen.self)
+                }
+            ]))
+//            self.gameMaster.pfsm.enter(GameMaster.GMSOptionsScreen.self)
         }
         if frontTouchedNode == "labelCredits" {
             self.gameMaster.pfsm.enter(GameMaster.GMSCreditsScreen.self)
